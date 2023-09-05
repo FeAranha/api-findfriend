bb295240750d1a4048130307c148c0bbb3ace08e
FeAranha <felipearanha.c@gmail.com>
2023-09-05 18:34:30 -0300

    change register to registerUser

--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -1,5 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { RegisterUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/registerUser'
 
 export function makeRegisterUseCase() {
   const usersRepository = new PrismaUsersRepository()
--- src/use-cases/registerUser.spec.ts
+++ src/use-cases/registerUser.spec.ts
@@ -1,6 +1,6 @@
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
-import { RegisterUseCase } from './register'
+import { RegisterUseCase } from './registerUser'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
